e0289768a72ea8f46558b7622d306574f48e07aa
HeroOfYore <aragornnlink@gmail.com>
2026-02-11 09:51:02 -0800

    amogus
    why won't you become red

--- UNBALATROABLE/reskins/UNCARDABLE_OTHER.lua
+++ UNBALATROABLE/reskins/UNCARDABLE_OTHER.lua
@@ -4,3 +4,8 @@
         loc_txt = {name = "Poco and Apoco"},
         boss_colour = HEX("CE2DE9"),
         }, true)
+
+   SMODS.Blind:take_ownership('bl_wall', {
+      loc_txt = {name = "{C:mult}Amogus{}"},
+      boss_colour = HEX("FFFFBB"),
+   }, true)
